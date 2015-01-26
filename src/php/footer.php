d4c63213347c8d8eec4c99fef06c2dfb5ad928b9
Cilyan Olowen <gaknar@gmail.com>
2015-01-26 01:47:34 +0100

    Rebrand footer
    Display the proper information about the theme name.
    
    Signed-off-by: Cilyan Olowen <gaknar@gmail.com>

--- src/php/footer.php
+++ src/php/footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'fictive' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'fictive' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'fictive' ), 'Fictive', '<a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'fictive' ), 'Fictive-Cilyan', '<a href="http://www.cilyan.org/" rel="designer">Cilyan</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
